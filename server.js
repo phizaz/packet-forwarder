4a9b8d53bc0327622322accff7f9b5605711c130
Konpat Preechakul <the.akita.ta@gmail.com>
2017-09-19 04:46:55 +0100

    error handing and stateful

--- server.js
+++ server.js
@@ -13,11 +13,26 @@ const OUTGOING = [
 ]
 
 /**
+ * keep state
+ */
+const state = {
+    lastHost: { host: null, port: null }
+}
+
+/**
  * START LISTENING
  */
 net.createServer(
     forwardTo(OUTGOING)
-).listen(INCOMING_PORT)
+)
+    .listen(INCOMING_PORT)
+    .on('error', err => {
+        if (err.errno == 'EADDRINUSE') {
+            console.error(`cannot bind the ${INCOMING_PORT} port`)
+        } else {
+            console.error('error while creating server:', err.errno, 'message:', err.message)
+        }
+    })
 
 function ping(host, port, timeout = 500) {
     return new Promise((res, rej) => {
@@ -35,11 +50,11 @@ function ping(host, port, timeout = 500) {
 }
 
 function forwardTo(list) {
-    /**
-     * ping and dynamically change the target host here
-     */
-    const pings = OUTGOING.map(x => ping(x.host, x.port))
     return (conn) => {
+        /**
+         * ping and dynamically change the target host here
+         */
+        const pings = OUTGOING.map(x => ping(x.host, x.port))
         /**
          * wait until the host is determined
          */
@@ -51,15 +66,41 @@ function forwardTo(list) {
                     if (okay) {
                         const host = out.host
                         const port = out.port
-                        console.log(`forwarding ${INCOMING_PORT} to ${host}:${port}`)
 
+                        if (host != state.lastHost.host
+                            || port != state.lastHost.port) {
+                            console.log(`forwarding ${INCOMING_PORT} to ${host}:${port}`)
+                        }
+
+                        state.lastHost = { host, port }
+
+                        /**
+                         * forward the packets
+                         */
                         const proxy = net.createConnection({ host, port }, () => {
                             conn.pipe(proxy).pipe(conn)
                         })
+                            .on('error', err => {
+                                /**
+                                 * clear state
+                                 */
+                                state.lastHost = { host: null, port: null }
+                                if (err.errno == 'ECONNRESET') {
+                                    console.error('connection reset ... check the internet connection')
+                                } else {
+                                    console.error('error while forwarding:', err.errno, 'message:', err.message)
+                                }
+                            })
 
-                        break
+                        return
                     }
                 }
+
+                /**
+                 * no host
+                 */
+                console.error('no host reachable!')
+                conn.end()
             })
     }
 }
